b88ccd59dcd1238539ba1dc19b168c6b6467a22b
armintavassoli14 <armintavassoli14@gmail.com>
2023-12-27 16:17:36 +0330

    add-Dto

---
+++ University/Dto/TeacherDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace University.Dto
+{
+    public class TeacherDto
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int NationalCode { get; set; }
+
+        public int? PhoneNumber { get; set; }
+    }
+}
