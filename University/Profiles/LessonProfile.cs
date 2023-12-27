1eb24fb595f33f933d75bb21dcecae8968deaab7
armintavassoli14 <armintavassoli14@gmail.com>
2023-12-27 16:29:39 +0330

    add-profile

---
+++ University/Profiles/LessonProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace University.Profiles
+{
+    public class LessonProfile : Profile
+    {
+        public LessonProfile()
+        {
+            CreateMap<Models.Lesson , Dto.LessonDto>();
+        }
+    }
+}
