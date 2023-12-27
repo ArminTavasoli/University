5b5bfdc46b323c5e42a43263b1c27aa4d04f92cb
armintavassoli14 <armintavassoli14@gmail.com>
2023-12-27 15:58:48 +0330

    Add -Auto- Mapper

---
+++ University/Controller/LessonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace University.Controller
+{
+    [Route("api/Lesson")]
+    [ApiController]
+    public class LessonController : ControllerBase
+    {
+    }
+}
