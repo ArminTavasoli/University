af95ed27a7025bef0df759626d945fe33f1a17e6
armintavassoli14 <armintavassoli14@gmail.com>
2023-12-27 15:20:43 +0330

    Create Repository

--- University/Migrations/20231227075209_Alter_table.Designer.cs
+++ University/Migrations/20231227075209_Alter_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using University.DbContexts;
 
@@ -11,9 +12,10 @@ using University.DbContexts;
 namespace University.Migrations
 {
     [DbContext(typeof(UniversityDbContext))]
-    partial class UniversityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231227075209_Alter_table")]
+    partial class Alter_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
